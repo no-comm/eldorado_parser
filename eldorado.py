3afa185b09c75a08464272271b0dd65685d69db8
danil32471 <67383667+danil32471@users.noreply.github.com>
2022-07-29 14:24:27 +0500

    Add files via upload

--- eldorado.py
+++ eldorado.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import config
-def collect():
-    response = requests.get('https://www.eldorado.ru/search/catalog.php?q=%D0%B0%D0%B2%D1%8B&utf', cookies=config.cookies, headers=config.headers)
+def collect(name):
+    response = requests.get(f'https://www.eldorado.ru/search/catalog.php?q={name}&utf', cookies=config.cookies, headers=config.headers)
     soup = bs(response.text, "lxml")
     prices = soup.find_all('span', class_="xS ES")
     names = soup.find_all("a", class_="FF")
